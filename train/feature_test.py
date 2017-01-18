79da7c0d6cd2155a99209febfb98cc6f2f36eb4c
cmrigney <crigney@sensourceinc.com>
2017-01-18 10:25:32 -0500

    Added test image.

--- train/feature_test.py
+++ train/feature_test.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 c = CircularHistogramDepthDifference(block_size=8, num_rings=3, ring_dist=10, num_blocks_first_ring=6)
 
-features, drawing = c.process_image('test.png', True)
+features, drawing = c.process_image('data/test/test.png', True)
 
 plt.imshow(drawing)
 plt.show()
