b29bf811be3c6a54706ba8a0f5de4d2d905da753
cmrigney <crigney@sensourceinc.com>
2017-01-23 09:43:32 -0500

    Fix circular histogram feature.

--- train/features/circular_histogram_depth_difference.py
+++ train/features/circular_histogram_depth_difference.py
@@ -39,7 +39,7 @@ class CircularHistogramDepthDifference(BaseFeature):
         if math.isnan(d):
           raise Exception('Uh oh')
         ring_features.append(max(min(d - d_center, 120), -80))
-      hist, edges = np.histogram(np.asarray(ring_features), bins=[-80 -20, 0, 10, 30, 50, 80, 120])
+      hist, edges = np.histogram(np.asarray(ring_features), bins=[-80, -20, 0, 10, 30, 50, 80, 120])
       features = np.concatenate((features, hist))
     if draw_regions:
       return features, drawing
